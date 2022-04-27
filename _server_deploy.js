d927dc78f24312b98e6324251b263dd85e552177
sharifulislam <shariful0401@gmail.com>
2022-04-28 03:38:36 +0600

    added hero api

--- _server_deploy.js
+++ _server_deploy.js
@@ -2,5 +2,17 @@
  * 1. create heroku account
  * 2. verify email
  * 3. install heroku cli
- * 4. 
+ * 4. ----------------
+ * make sure : git add . git commit . git push
+ * git push heroku main
+ * go to heroku dashbord > current project > setting> reveal config vars
+ * copy paste config vars from your .env file
+ * make sure you have whitelisted all ip address to access mongodb
+ * ---------------------------
+ * update server
+ * --------------
+ * make changes
+ * make sure : git add . git commit . git push
+ *  git push heroku main
+ * ------
 */
